997da6118fda9968eb388228add59ca0552dd234
FlorianeJN <132172528+FlorianeJN@users.noreply.github.com>
2025-04-18 14:54:35 -0400

    Added data fetching for invoices card

--- components/invoices-stats.tsx
+++ components/invoices-stats.tsx
@@ -1,31 +1,55 @@
+import {
+  fetchInvoiceStats2025,
+  fetchTotalAmounts,
+  fetchTotalPendingAmounts,
+  fetchTotalReceivedAmounts,
+} from "@/lib/data";
 import Tuile from "./custom/tuile";
 
-export function InvoicesStats() {
+export async function InvoicesStats() {
+  const { total, byPartner } = await fetchInvoiceStats2025();
+  const { total: totalReceived, byPartner: byPartnerReceived } =
+    await fetchTotalReceivedAmounts();
+  const { total: totalPending, byPartner: byPartnerPending } =
+    await fetchTotalPendingAmounts();
+
+  const { total: totalAmount, byPartner: byPartnerTotalAmount } =
+    await fetchTotalAmounts();
+
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:grid-cols-2 @xl/main:grid-cols-4">
       <Tuile
-        title="Nombre total des factures générées en 2025"
-        value="123"
+        title="Nombre total des factures générées"
+        value={total.toString()}
         code={
           <>
             <div className="line-clamp-1 flex gap-2 font-medium">
-              CHSLD Granby : 2
+              CHSLD Saint-Jean :{" "}
+              {byPartner.find((p) => p.partner === "CHSLD Saint-Jean")?.count}
             </div>
-            <div className="text-muted-foreground"> CHSLD Saint-Jean : 4</div>
+            <div className="text-muted-foreground">
+              {" "}
+              CHSLD Granby :{" "}
+              {byPartner.find((p) => p.partner === "CHSLD Granby")?.count}
+            </div>
           </>
         }
       />
 
       <Tuile
-        title="Montant total reçu en 2025"
-        value="12 034$"
+        title="Montant total reçu "
+        value={totalReceived.toString()}
         code={
           <>
             <div className="line-clamp-1 flex gap-2 font-medium">
-              CHSLD Granby : 2345$
+              CHSLD Saint-Jean :{" "}
+              {byPartnerReceived.find((p) => p.partner === "CHSLD Saint-Jean")
+                ?.total || "0 $"}
             </div>
             <div className="text-muted-foreground">
-              CHSLD Saint-Jean : 9689$
+              CHSLD Granby :{" "}
+              {byPartnerReceived.find((p) => p.partner === "CHSLD Granby")
+                ?.total || "0 $"}
             </div>
           </>
         }
@@ -33,14 +57,18 @@ export function InvoicesStats() {
 
       <Tuile
         title="Montant en attente de paiement"
-        value="12 034$"
+        value={totalPending.toString()}
         code={
           <>
             <div className="line-clamp-1 flex gap-2 font-medium">
-              Factures prêtes : 444$
+              CHSLD Saint-Jean :{" "}
+              {byPartnerPending.find((p) => p.partner === "CHSLD Saint-Jean")
+                ?.total || "0 $"}
             </div>
             <div className="text-muted-foreground">
-              Factures à compléter : 1234$
+              CHSLD Granby :{" "}
+              {byPartnerPending.find((p) => p.partner === "CHSLD Granby")
+                ?.total || "0 $"}
             </div>
           </>
         }
@@ -48,14 +76,23 @@ export function InvoicesStats() {
 
       <Tuile
         title="Montant total facturé"
-        value="33 435$"
+        value={totalAmount}
         code={
           <>
             <div className="line-clamp-1 flex gap-2 font-medium">
-              CHSLD Granby : 2564$
+              CHSLD Saint-Jean :{" "}
+              {
+                byPartnerTotalAmount.find(
+                  (p) => p.partner === "CHSLD Saint-Jean"
+                )?.total
+              }
             </div>
             <div className="text-muted-foreground">
-              CHSLD Saint-Jean : 4536$
+              CHSLD Granby :{" "}
+              {
+                byPartnerTotalAmount.find((p) => p.partner === "CHSLD Granby")
+                  ?.total
+              }
             </div>
           </>
         }
--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,12 @@ import { Employee, Invoice, Partner, Shift } from "./definitions";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
+const formatter = new Intl.NumberFormat("fr-CA", {
+  style: "currency",
+  currency: "CAD",
+  minimumFractionDigits: 2,
+});
+
 export async function fetchPartners() {
   try {
     const data = await sql<Partner[]>`SELECT * FROM partenaire`;
@@ -46,5 +52,148 @@ export async function fetchInvoice(num_facture: string) {
   } catch (e) {
     console.log(e);
     throw new Error("Database Error Fetching Invoice");
+  }
+}
+
+export async function fetchInvoiceStats2025() {
+  try {
+    // Total number of invoices in 2025
+    const totalInvoices = await sql<{ count: string }[]>`
+      SELECT COUNT(*) FROM facture
+     
+    `;
+
+    // Number of invoices per partner in 2025
+    const invoicesByPartner = await sql<
+      { nom_partenaire: string; count: string }[]
+    >`
+      SELECT nom_partenaire, COUNT(*) 
+      FROM facture
+      
+      GROUP BY nom_partenaire
+    `;
+
+    return {
+      total: Number(totalInvoices[0].count),
+      byPartner: invoicesByPartner.map((row) => ({
+        partner: row.nom_partenaire,
+        count: Number(row.count),
+      })),
+    };
+  } catch (e) {
+    console.error(e);
+    throw new Error("Database Error Fetching Invoice Stats");
+  }
+}
+
+export async function fetchTotalAmounts() {
+  try {
+    // Montant total global
+    const totalResult = await sql<{ total: number | null }[]>`
+      SELECT SUM(montant_apres_taxes) AS total
+      FROM facture
+    `;
+
+    const total = totalResult[0]?.total ?? 0;
+
+    // Montant total par partenaire
+    const partnerResult = await sql<
+      { nom_partenaire: string; total: number | null }[]
+    >`
+      SELECT nom_partenaire, SUM(montant_apres_taxes) AS total
+      FROM facture
+      GROUP BY nom_partenaire
+    `;
+
+    const byPartner = partnerResult.map((row) => ({
+      partner: row.nom_partenaire,
+      total: formatter.format(row.total ?? 0),
+    }));
+
+    return {
+      total: formatter.format(total),
+      byPartner,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching total invoice amounts");
+  }
+}
+
+export async function fetchTotalReceivedAmounts() {
+  try {
+    // Montant total payé
+    const totalResult = await sql<{ total: number }[]>`
+      SELECT SUM(montant_apres_taxes) AS total
+      FROM facture
+      WHERE statut = 'Payée'
+    `;
+
+    const total = totalResult[0].total || 0;
+
+    // Montant payé par partenaire
+    const partnerResult = await sql<
+      { nom_partenaire: string; total: number }[]
+    >`
+      SELECT nom_partenaire, SUM(montant_apres_taxes) AS total
+      FROM facture
+      WHERE statut = 'Payée'
+      GROUP BY nom_partenaire
+    `;
+
+    const byPartner = partnerResult.map((row) => ({
+      partner: row.nom_partenaire,
+      total: formatter.format(row.total || 0),
+    }));
+
+    return {
+      total: formatter.format(total),
+      byPartner,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching total received amounts");
+  }
+}
+
+export async function fetchTotalPendingAmounts() {
+  try {
+    const formatter = new Intl.NumberFormat("fr-CA", {
+      style: "currency",
+      currency: "CAD",
+      minimumFractionDigits: 2,
+    });
+
+    // Montant en attente
+    const totalResult = await sql<{ total: number | null }[]>`
+      SELECT SUM(montant_apres_taxes) AS total
+      FROM facture
+      WHERE statut = 'Envoyée'
+    `;
+
+    const total = totalResult[0]?.total ?? 0;
+
+    // Montant en attente par partenaire
+    const partnerResult = await sql<
+      { nom_partenaire: string; total: number | null }[]
+    >`
+      SELECT nom_partenaire, SUM(montant_apres_taxes) AS total
+      FROM facture
+      WHERE statut = 'Envoyée'
+      GROUP BY nom_partenaire
+    `;
+
+    const byPartner = partnerResult.map((row) => ({
+      partner: row.nom_partenaire,
+      total: formatter.format(row.total ?? 0),
+    }));
+
+    return {
+      total: formatter.format(total),
+      byPartner,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching total pending amounts");
   }
 }
